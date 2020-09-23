1c369056f3b4e66ef5550e8ca5e2810dd9a9fe3f
Susanta <sdeka@bgsu.edu>
2020-09-23 14:45:03 -0400

    moment installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
+import Lyrics from './components/tracks/Lyrics'
 import { Provider } from './context'
 import './App.css';
 
@@ -11,13 +13,12 @@ function App() {
       <Router>
         <React.Fragment>
           <Navbar />
-          <div className="container-bg">
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
-          </div>
         </React.Fragment>
         </Router>
     </Provider>
