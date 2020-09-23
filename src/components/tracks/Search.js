e51ea2ed575185a9ce834afd9c0399c6d5cc303e
Susanta <sdeka@bgsu.edu>
2020-09-23 18:43:22 -0400

    Pre Hooks

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -8,17 +8,33 @@ class Search extends Component {
         trackTitle : ''
     }
 
+    getInfo = (dispatch,e) => {
+        e.preventDefault()
+
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search.get?q=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
+            .then(res => {
+                dispatch({
+                    type: 'SEARCH',
+                    payload: res.data.message.body.track_list
+                });
+                this.setState({trackTitle: ''})
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <Consumer>
                 {value => {
+                    const { dispatch } = value;
+                    console.log(dispatch)
                     return (
                         <div className="card card-body mb-4 p-4">
                             <h1 className="display-4 text-center">
-                                <i className="fas fa-music"></i>Search for song
+                                <i className="fas fa-music"></i> Get Music Info
                             </h1>
-                            <p className="lead text-center">Get info for song</p>
-                            <form>
+                            <p className="lead text-center">Get info for track</p>
+                            <form onSubmit={this.getInfo.bind(this, dispatch)}>
                                 <div className="form=group">
                                     <input 
                                     type="text" 
@@ -29,6 +45,7 @@ class Search extends Component {
                                     onChange={(e) => this.setState({trackTitle: e.target.value})}
                                     />
                                 </div>
+                                <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">Get Info</button>
                             </form>
                         </div>
                     )
--- src/context.js
+++ src/context.js
@@ -3,11 +3,25 @@ import axios from 'axios'
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'SEARCH':
+            return {
+                ...state,
+                track_list: action.payload,
+                heading: 'Search Results'
+            };
+        default:
+            return state;
+    }
+}
+
 export class Provider extends Component {
 
     state = {
         track_list: [],
-        heading: 'Top Tracks'
+        heading: 'Top Tracks',
+        dispatch: action => this.setState(state => reducer(state, action))
     }
 
     componentDidMount() {
